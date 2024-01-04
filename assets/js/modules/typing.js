1b882c50e755335eaf7be43f79f8bbd76d98d7a3
andrezambroni <andrezambroni@gmail.com>
2024-01-04 19:36:37 -0300

    fix scripts

--- assets/js/modules/typing.js
+++ assets/js/modules/typing.js
@@ -1,12 +1,14 @@
 export default function initTyping() {
     let letter = 0;
     const text = '     André Zambroni';
+    const sentenceElement = document.querySelector(".principal__sentence");
+    const textArray = Array.from(text);
 
     function typeWriter() {
-        if(letter < text.length) {
-            document.querySelector(".principal__sentence").innerHTML += text.charAt(letter);
+        if (letter < textArray.length) {
+            sentenceElement.textContent += textArray[letter];
             letter++;
-            setTimeout(typeWriter, 180)
+            setTimeout(typeWriter, 180);
         }
     }
 
